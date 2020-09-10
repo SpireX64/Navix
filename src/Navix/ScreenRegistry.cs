c1c9bc39d6679d3a876dab3e31e664332db42c48
SpireX <SpireX@outlook.com>
2020-09-10 20:54:12 +0200

    [task] Добавлен регистер экранов

---
+++ src/Navix/ScreenRegistry.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Concurrent;
+
+namespace Spx.Navix
+{
+    public class ScreenRegistry
+    {
+        private readonly ConcurrentDictionary<int, object>
+            _screenResolversMap = new ConcurrentDictionary<int, object>();
+
+        public bool IsEmpty => _screenResolversMap.IsEmpty;
+
+        public void Register<TScreen>(IScreenResolver<TScreen> resolver) where TScreen : Screen
+        {
+            var screenTypeHash = typeof(TScreen).GetHashCode();
+            _screenResolversMap.TryAdd(screenTypeHash, resolver);
+        }
+
+        public bool HasScreen<TScreen>() where TScreen : Screen
+        {
+            var screenTypeHash = typeof(TScreen).GetHashCode();
+            return _screenResolversMap.ContainsKey(screenTypeHash);
+        }
+
+        public IScreenResolver<TScreen>? Resolve<TScreen>() where TScreen : Screen
+        {
+            var screenTypeHash = typeof(TScreen).GetHashCode();
+            if (_screenResolversMap.TryGetValue(screenTypeHash, out var resolver))
+                return (IScreenResolver<TScreen>) resolver;
+            else
+                return null;
+        }
+    }
+}
