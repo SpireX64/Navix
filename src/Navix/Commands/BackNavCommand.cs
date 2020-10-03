8876221488d485034e5c5fa5d4f353e4d4566644
SpireX <SpireX@outlook.com>
2020-10-03 17:37:38 +0200

    [task] Добавлены команды навигации

---
+++ src/Navix/Commands/BackNavCommand.cs
@@ -0,0 +1,12 @@
+﻿using Spx.Navix.Platform;
+
+namespace Spx.Navix.Commands
+{
+    public sealed class BackNavCommand : INavCommand
+    {
+        public void Apply(Navigator navigator)
+        {
+            navigator.Back();
+        }
+    }
+}
