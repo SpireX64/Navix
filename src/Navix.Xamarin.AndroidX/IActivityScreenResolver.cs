2418eb71a5380523416a3fbc4e31074e41b29c1e
SpireX <SpireX@outlook.com>
2020-10-11 23:33:40 +0200

    [fix] Справлены проблемы найденные в PR

--- src/Navix.Xamarin.AndroidX/IActivityScreenResolver.cs
+++ src/Navix.Xamarin.AndroidX/IActivityScreenResolver.cs
@@ -1,9 +1,11 @@
 ﻿using Android.Content;
+using AndroidX.Annotations;
 
 namespace Spx.Navix.Xamarin.AndroidX
 {
     public interface IActivityScreenResolver : IScreenResolver
     {
+        [NonNull]
         Intent GetActivityIntent(Screen screen, Context context);
     }
 }
