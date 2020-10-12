a76d73175acd01d94ae0bb52ac48332bcf9721ea
SpireX <SpireX@outlook.com>
2020-10-12 19:45:51 +0200

    [task] Обновлены свойства проектов Android Support и Android Legacy

--- src/Navix.Xamarin.AndroidSupport/Properties/AssemblyInfo.cs
+++ src/Navix.Xamarin.AndroidSupport/Properties/AssemblyInfo.cs
@@ -1,26 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using Android.App;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Navix.Xamarin.AndroidSupport")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Navix library for Xamarin Android Support v7")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("SpireX (Artem Sobolenkov)")]
 [assembly: AssemblyProduct("Navix.Xamarin.AndroidSupport")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("SpireX © 2020")]
+[assembly: AssemblyTrademark("Navix")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]
