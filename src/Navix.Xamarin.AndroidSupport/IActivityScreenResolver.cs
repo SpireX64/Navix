d054f333346dc15437c8cff4a8cc6dcd765581c2
SpireX <SpireX@outlook.com>
2020-10-12 19:47:42 +0200

    [bugfix] Добавлены пропущенные аттрибуты NonNull

--- src/Navix.Xamarin.AndroidSupport/IActivityScreenResolver.cs
+++ src/Navix.Xamarin.AndroidSupport/IActivityScreenResolver.cs
@@ -1,9 +1,11 @@
 ﻿using Android.Content;
+using Android.Support.Annotation;
 
 namespace Spx.Navix.Xamarin.AndroidSupport
 {
     public interface IActivityScreenResolver: IScreenResolver
     {
+        [NonNull]
         Intent GetActivityIntent(Screen screen, Context context);
     }
 }
--- src/Navix.Xamarin.AndroidSupport/IFragmentScreenResolver.cs
+++ src/Navix.Xamarin.AndroidSupport/IFragmentScreenResolver.cs
@@ -1,9 +1,11 @@
-﻿using Android.Support.V4.App;
+﻿using Android.Support.Annotation;
+using Android.Support.V4.App;
 
 namespace Spx.Navix.Xamarin.AndroidSupport
 {
     public interface IFragmentScreenResolver: IScreenResolver
     {
+        [NonNull]
         Fragment GetFragment(Screen screen);
     }
 }
